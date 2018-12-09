882f269e99205c844ca0492a9913b2ef47c573a5
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-12-09 18:24:26 +0000

    don't need to transpile govuk-frontend-react

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withSass = require('@zeit/next-sass')
 const withTM = require('next-plugin-transpile-modules');
 
 module.exports = withTM(withSass({
-  transpileModules: ['govuk-frontend-react', 'govuk-frontend'],
+  transpileModules: ['govuk-frontend'],
   webpack: (config, options) => {
     config.node = {
       fs: 'empty'
