c4553806e50430a08fff2a39a3b2258a737b59be
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-12-10 20:45:20 +0000

    Fix nav links

--- components/nav.js
+++ components/nav.js
@@ -2,14 +2,16 @@ import React from 'react'
 import Link from 'next/link'
 import { Header } from "govuk-frontend-react";
 
+const NextLink = ({href, ...props}) => <Link href={href}><a {...props} /></Link>
+
 const Nav = () => (
   <Header
     productName="React"
-    homepage={{ href: '/', as: (props) => <Link><a {...props} /></Link> }}
+    homepage={{ href: '/', as: NextLink }}
     navigation={(
       <Header.Navigation>
-        <Header.NavigationItem href="/">Home</Header.NavigationItem>
-        <Header.NavigationItem href="/about">About</Header.NavigationItem>
+        <Header.NavigationItem as={NextLink} href="/">Home</Header.NavigationItem>
+        <Header.NavigationItem as={NextLink} href="/about">About</Header.NavigationItem>
         <Header.NavigationItem href="https://github.com/penx/govuk-frontend-react-example">GitHub</Header.NavigationItem>
       </Header.Navigation>
     )}
